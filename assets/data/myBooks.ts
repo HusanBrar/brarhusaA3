6df7dbee7f94131711d3448b5b432edb08503f7e
HusanBrar <52754753+HusanBrar@users.noreply.github.com>
2019-07-26 13:36:03 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,8 @@
+import {Books} from '../../app/bookInfo';
+export const MYBOOKS: Books[] = 
+[
+    {bname: 'New York In Bloom', bauthor: 'Georgianna Lane', bgenre: 'Art and Architecture', byearPublished: 2019, bpicture:'assets/images/NewYorkInBloom.jpg'},
+    {bname: 'Becoming', bauthor: 'Michelle Obama', bgenre: 'Biography & Memoir', byearPublished: 2018, bpicture:'assets/images/Becoming.jpg'},
+    {bname: 'Dear Wife: A Novel', bauthor: 'Kimberly Belle', bgenre: 'Thrillers', byearPublished: 2019, bpicture:'assets/images/DearWife.jpg'},
+    {bname: 'Big Sky', bauthor: 'Kate Atkinson', bgenre: 'Fiction and Literature', byearPublished: 2019, bpicture:'assets/images/KateAtkinson.jpg'}
+]
